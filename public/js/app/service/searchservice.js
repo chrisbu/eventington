51ae711547a8548ca6c198b355f0027a8e622007
Chris Buckett <chrisbuckett@gmail.com>
2011-07-22 08:22:45 +0100

    Updated more service stuff

---
+++ public/js/app/service/searchservice.js
@@ -0,0 +1,48 @@
+(function(eventington, $, undefined) {
+  
+  /*
+   * Create the service namespace within eventington.
+   */
+  (function(searchservice, $, undefined) {
+  	
+  	    /*
+     * Returns a list of locations which match the partialName parameter.
+     * Useful for autocomplete.
+     * 
+     * @param err
+     *  - error callback
+     * @param success 
+     *  - success callback
+     * @param partialName  
+     */
+    searchservice.searchLocation = function(err, success, partialName) {
+      
+      var onError = function(data) {
+        err(data);  
+      };
+      
+      var onSuccess = function(data) {
+        success(data);
+      };
+      
+      //pass the call down to the dao layer.
+      eventington.serverdao.searchLocation(onError, onSuccess, locationString);
+    };
+    
+    searchservice.searchEvents = function(err, success, location, startDate, endDate) {
+    	
+    	var onError = function(data) {
+    		err(data);
+    	};
+    	
+    	var onSuccess = function(events) {
+    		success(events);
+    	};
+    	
+    	eventington.serverdao.searchEvents(onError, onSuccess, location, startDate, endDate);
+    };
+    
+  	
+  }(eventington.searchservice = eventington.searchservice || {}, $)); //self executing anonymous functon to create the namespace methods
+
+}(window.eventington = window.eventington || {}, jQuery)); //self executing anonymous functon to create the namespace methods
---
+++ public/js/app/ui/eventshandler.js
@@ -0,0 +1,13 @@
+(function(eventington, $, undefined) {
+  
+    (function(ui, $, undefined) {
+      
+      ui.showEvents = function(events) {
+      	
+      };
+      
+     
+      
+    }(eventington.ui = eventington.ui || {}, $)); //self executing anonymous functon to create the namespace methods
+
+}(window.eventington = window.eventington || {}, jQuery)); //self executing anonymous functon to create the namespace methods
